c3ccf1edd1476c8d652405f4c035ebfc47a33896
Jocelyn-JE <jocelyn.jean-elie@epitech.eu>
2025-03-06 16:15:39 +0100

    ✨ Implemented PWD command in Client class 🚸 Enhance user authentication flow in Client class

--- src/server/Client.cpp
+++ src/server/Client.cpp
@@ -34,32 +34,58 @@ static std::string clientDisconnect(std::string commandLine,
 
 static std::string doUser(std::string commandLine, ftp::Client &client)
 {
+    std::string tempUser;
+
     if (commandLine.size() < 6 || commandLine.substr(0, 5) != "USER ")
         return "501 Syntax error in parameters or arguments.";
-    client._username = commandLine.substr(5, commandLine.size() - 7);
+    tempUser = commandLine.substr(5);
+    printf("User: %s\n", tempUser.c_str());
+    if (tempUser != "Anonymous")
+        return "530 Not logged in.";
+    client._username = tempUser;
     return "331 User name okay, need password.";
+}
+
+static std::string checkLogin(ftp::Client &client)
+{
+    if (client.isLoggedIn())
+        return "230 User logged in, proceed.";
+    return "530 Not logged in.";
 }
 
 static std::string doPass(std::string commandLine, ftp::Client &client)
 {
+    if (client._username == "")
+        return "332 Need account for login.";
     if (commandLine.size() < 5 || commandLine.substr(0, 5) != "PASS ")
         return "501 Syntax error in parameters or arguments.";
-    if (client._username == "") {
+    if (client._username == "")
         return "503 Bad sequence of commands.";
-    }
-    client._password = commandLine.substr(5, commandLine.size() - 7);
-    return "230 User logged in, proceed.";
+    client._password = commandLine.substr(5);
+    return checkLogin(client);
+}
+
+static std::string doPwd(std::string commandLine, ftp::Client &client)
+{
+    if (!client.isLoggedIn())
+        return "530 Not logged in.";
+    if (commandLine != "PWD")
+        return "501 Syntax error in parameters or arguments.";
+    return "257 \"" + client._currentPath + "\" is the current directory.";
 }
 
 //-----------------------------------------------------------------------------
 
-ftp::Client::Client(int fd, struct sockaddr_in address) : _socket(fd, address)
+ftp::Client::Client(int fd, struct sockaddr_in address, std::string rootPath)
+    : _socket(fd, address), _username(""), _password(""),
+    _currentPath("/"), _rootPath(rootPath)
 {
     _commands[""] = clientDisconnect;
     _commands["NOOP"] = doNoop;
     _commands["QUIT"] = doQuit;
     _commands["USER"] = doUser;
     _commands["PASS"] = doPass;
+    _commands["PWD"] = doPwd;
 }
 
 ftp::Client::~Client()
@@ -102,4 +128,11 @@ void ftp::Client::handleCommand(std::string commandLine)
         commandLine.substr(0, commandLine.size() - 2), *this));
     if (commandLine == "QUIT\r\n")
         _socket.closeSocket();
+}
+
+bool ftp::Client::isLoggedIn() const
+{
+    if (_username == "Anonymous" && _password == "")
+        return true;
+    return false;
 }
