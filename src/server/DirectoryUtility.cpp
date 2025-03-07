d0583b48469c6d60f5b9a4dc5092325652b85ddb
Jocelyn-JE <jocelyn.jean-elie@epitech.eu>
2025-03-07 15:11:53 +0100

    🐛 Fix getParentDirectory to handle paths that start with a slash

--- src/server/DirectoryUtility.cpp
+++ src/server/DirectoryUtility.cpp
@@ -10,7 +10,7 @@
 std::string ftp::DirectoryUtility::getParentDirectory(const std::string &path)
 {
     size_t lastSlash = path.find_last_of('/');
-    if (lastSlash == std::string::npos)
+    if (lastSlash == std::string::npos || lastSlash == 0)
         return "/";
     return path.substr(0, lastSlash);
 }
