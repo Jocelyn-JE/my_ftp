6fc56b0f029e219eb479d078e2479f08a669f731
Jocelyn-JE <jocelyn.jean-elie@epitech.eu>
2025-03-14 14:24:36 +0100

    🎨 Converted DirectoryUtility namespace to static class

--- include/DirectoryUtility.hpp
+++ include/DirectoryUtility.hpp
@@ -13,16 +13,17 @@
     #include "Socket.hpp"
 
 namespace ftp {
-namespace DirectoryUtility {
-std::string const &removeTrailingSlash(std::string *path);
-std::string resolvePath(std::string const &root,
-std::string const &currentDirectory, std::string const &path);
-std::string resolveCanonicalPath(std::string const &root,
-std::string const &currentDirectory, std::string const &path);
-std::string getParentDirectory(const std::string &path);
-bool dirExists(const std::string &path);
-bool fileExists(const std::string &path);
-}
+class DirectoryUtility {
+ public:
+    static std::string const &removeTrailingSlash(std::string *path);
+    static std::string resolvePath(std::string const &root,
+        std::string const &currentDirectory, std::string const &path);
+    static std::string resolveCanonicalPath(std::string const &root,
+        std::string const &currentDirectory, std::string const &path);
+    static std::string getParentDirectory(const std::string &path);
+    static bool dirExists(const std::string &path);
+    static bool fileExists(const std::string &path);
+};
 }  // namespace ftp
 
 #endif  // DIRECTORYUTILITY_HPP_
